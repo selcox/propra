15a16f86f4ce675d688104976621711cac5e7523
Sergio Muñoz <sergio.munoz.lillo@gmail.com>
2020-01-23 18:09:29 +0100

    Added some interface to exercise 1

--- src/propra/com/sml/programacion/practicas/entregas/febrero7/ejercicio1/Ejercicio1.java
+++ src/propra/com/sml/programacion/practicas/entregas/febrero7/ejercicio1/Ejercicio1.java
@@ -1,14 +1,23 @@
 package propra.com.sml.programacion.practicas.entregas.febrero7.ejercicio1;
 
-import java.awt.BorderLayout;
-import javax.swing.JButton;
-import javax.swing.JList;
-import javax.swing.JPanel;
-import javax.swing.JTextField;
+import javax.swing.JOptionPane;
+
 import propra.com.sml.programacion.utilities.graphics.Frame;
 
 public class Ejercicio1 {
 	public static void main (String[] args) {
-		Frame window = new Frame("Mi diccionario");
+//		int nTimes=Integer.parseInt(JOptionPane.showInputDialog("¿Cuántas palabras quieres ordenar?"));
+//		String[] unorderedWords = new String[nTimes];
+//		for(int i=0;i<nTimes;i++) {
+//			unorderedWords[i]=JOptionPane.showInputDialog("Inserta una palabra a añadir");
+//		}
+		int width;
+		JOptionPane.showMessageDialog(null, "He ordenado tus palabras, a la izquierda lo desordenado, a la derecha lo ordenado");
+		Frame unordered = new Frame("Palabras desordenadas");
+		Frame ordered = new Frame("Palabras ordenadas");
+		width=ordered.getWIDTH();
+		unordered.setLocation(0,0);			//Coloca ambos frames alineados
+		ordered.setLocation(width,0);       //  el uno con el otro 
+
 	}
 }
--- src/propra/com/sml/programacion/utilities/graphics/Frame.java
+++ src/propra/com/sml/programacion/utilities/graphics/Frame.java
@@ -1,16 +1,21 @@
 package propra.com.sml.programacion.utilities.graphics;
 
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
 import java.awt.Toolkit;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class Frame extends JFrame {
 	public static final int WIDTH = Toolkit.getDefaultToolkit().getScreenSize().width/2;
-	public static final int HEIGHT = Toolkit.getDefaultToolkit().getScreenSize().height/2;
+	public static final int HEIGHT = Toolkit.getDefaultToolkit().getScreenSize().height;
 	public Frame(String title) {
 		setSize(WIDTH, HEIGHT);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setVisible(true);
-		setLocationRelativeTo(null);
 		setTitle(title);
 	}
 	public int getWIDTH() {
