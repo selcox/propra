297de48c81acb1bfd004d391404338f0616baa6a
Sergio Muñoz <sergio.munoz.lillo@gmail.com>
2020-01-31 20:34:59 +0100

    Exercise two need to be finished, and exercise one needs a revision

--- src/propra/com/sml/programacion/practicas/entregas/febrero7/ejercicio2/Ejercicio2.java
+++ src/propra/com/sml/programacion/practicas/entregas/febrero7/ejercicio2/Ejercicio2.java
@@ -1,27 +1,67 @@
 package propra.com.sml.programacion.practicas.entregas.febrero7.ejercicio2;
 
-import java.awt.Toolkit;
 import java.util.Random;
 
 public class Ejercicio2 {
+	static Random selector = new Random();
+	static String[] company = {"IBM","Ubisoft","Microsoft","Mojang","Bethesda","Electronic Arts","Nintendo","Konami","Activision","Valve"};
+	static String[] employees = {"Juan","María","Sergio","Federico","Amy","Pablo","Leandro","Susana","Lorena","Julio","Beatriz","Julia","Paula","Adrián","James","David","Nuria","Pedro","Anabel","Luisa","Juanjo"};
+	static int[][][] cube = new int[3][4][6];
+	static String[][] auxEmployees = new String[3][4];
+	static String company1="";
+	static String company2="";
+	static String company3="";
+	static int sumSalaries;
 	public static void main(String[] args) {
-		String[] company = {"IBM","Ubisoft","Microsoft","Mojang","Bethesda","Electronic Arts","Nintendo","Konami","Activision","Valve"};
-		String[] employees = {"Juan","María","Sergio","Federico","Amy","Pablo","Leandro","Susana","Lorena","Julio","Beatriz","Julia","Paula","Adrián","James","David"};
-		String[][][] cube = new String[3][4][5];
-		int height = (int) (0.5*0.95*Toolkit.getDefaultToolkit().getScreenSize().height);
-		int width = (int) (0.5*Toolkit.getDefaultToolkit().getScreenSize().width);
-		fillCube(cube);
-		Ex2Graphics window = new Ex2Graphics(width,height,cube,company,employees);
+		fillSalaries(cube);
+		fillCompanies(company1,company2,company3);
+		fillEmployees();
+		initFrame();
+	}
+	public static void initFrame() {
+		Ex2Graphics window = new Ex2Graphics();
+		//window.setTextArea(area1, area2, area3);
 		window.setVisible(true);
 	}
-	public static void fillCube(String[][][] cube) {
-		Random selector = new Random();
+	public static void fillSalaries(int[][][] cube) {
 		for(int i=0;i<cube.length;i++) {
 			for(int j=0;j<cube[0].length;j++) {
+				sumSalaries=0;
 				for(int k=0;k<cube[0][0].length;k++) {
-					cube[i][j][k]=Integer.toString(selector.nextInt(3501)+1000);
+					if(k==(cube[0][0].length-1)) {
+						cube[i][j][k]=sumSalaries/5;
+					} else {
+						cube[i][j][k]=selector.nextInt(3501)+1000;
+						sumSalaries+=cube[i][j][k];
+					}
 				}
 			}
+		}
+	}
+	public static void fillCompanies(String name1, String name2, String name3) {
+		String constant="\nEmpresa: ";
+		while (name1.equals(name2) || name2.equals(name3) || name1.equals(name3)) {
+			name1=constant+company[selector.nextInt(company.length)];
+			name2=constant+company[selector.nextInt(company.length)];
+			name3=constant+company[selector.nextInt(company.length)];
+		}
+	}
+	public static void fillEmployees() {
+		String name1="";
+		String name2="";
+		String name3="";
+		String name4="";
+		for(int i=0;i<auxEmployees.length;i++) {
+			while (name1.equals(name2) || name1.equals(name3) || name1.equals(name4) || name2.equals(name3) || name2.equals(name4) || name3.equals(name4)) {
+				name1=employees[selector.nextInt(employees.length)];
+				name2=employees[selector.nextInt(employees.length)];
+				name3=employees[selector.nextInt(employees.length)];
+				name4=employees[selector.nextInt(employees.length)];
+			}
+			auxEmployees[i][0]=name1;
+			auxEmployees[i][1]=name2;
+			auxEmployees[i][2]=name3;
+			auxEmployees[i][3]=name4;
 		}
 	}
 }
--- src/propra/com/sml/programacion/practicas/entregas/febrero7/ejercicio2/Ex2Graphics.java
+++ src/propra/com/sml/programacion/practicas/entregas/febrero7/ejercicio2/Ex2Graphics.java
@@ -1,61 +1,35 @@
 package propra.com.sml.programacion.practicas.entregas.febrero7.ejercicio2;
 
-import java.awt.BorderLayout;
-import java.util.Random;
+import java.awt.GridLayout;
+import java.awt.Toolkit;
 import javax.swing.JFrame;
 import javax.swing.JTextArea;
 
 public class Ex2Graphics extends JFrame{
+	static final int HEIGHT = (int) (0.5*0.95*Toolkit.getDefaultToolkit().getScreenSize().height);
+	static final int WIDTH = (int) (0.5*Toolkit.getDefaultToolkit().getScreenSize().width);
 	private static final long serialVersionUID = 1L;
-	public Ex2Graphics(int width, int height, String[][][] cube, String[] company, String[] employees) {
-		setSize(width,height);
+	public Ex2Graphics() {
+		setSize(WIDTH,HEIGHT);
 		setResizable(false);
-		setLocation(width/2,height/2);
+		setLocation(WIDTH/2,HEIGHT/2);
 		setTitle("Empresas");
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		setLayout(new BorderLayout());
-		int paneHeight = height/3;
-		Random selector = new Random();
-		String textP1="";
-		String textP2="";
-		String textP3="";
-		int avg=0;
-		while (textP1.equals(textP2) || textP2.equals(textP3) || textP1.equals(textP3)) {
-			textP1="\n Empresa: "+company[selector.nextInt(company.length)];
-			textP2="\n\n\n\n Empresa: "+company[selector.nextInt(company.length)];
-			textP3="\n Empresa: "+company[selector.nextInt(company.length)];
-		}
-		for(int i=0;i<4;i++) {
-			for(int j=0;j<5;j++) {
-				avg+=Integer.valueOf(cube[0][i][j]);
-			}
-			avg=avg/5;
-			textP1+="\n     "+employees[selector.nextInt(employees.length)]+" ha cobrado: "+cube[0][i][0]+"€ , "+cube[0][i][1]+"€ , "+cube[0][i][2]+"€ , "+cube[0][i][3]+"€ , "+cube[0][i][4]+"€ , de media: "+avg+"€";
-			avg=0;
-			for(int j=0;j<5;j++) {
-				avg+=Integer.valueOf(cube[1][i][j]);
-			}
-			avg=avg/5;
-			textP2+="\n     "+employees[selector.nextInt(employees.length)]+" ha cobrado: "+cube[1][i][0]+"€ , "+cube[1][i][1]+"€ , "+cube[1][i][2]+"€ , "+cube[1][i][3]+"€ , "+cube[1][i][4]+"€ , de media: "+avg+"€";
-			avg=0;
-			for(int j=0;j<5;j++) {
-				avg+=Integer.valueOf(cube[2][i][j]);
-			}
-			avg=avg/5;
-			textP3+="\n     "+employees[selector.nextInt(employees.length)]+" ha cobrado: "+cube[2][i][0]+"€ , "+cube[2][i][1]+"€ , "+cube[2][i][2]+"€ , "+cube[2][i][3]+"€ , "+cube[2][i][4]+"€ , de media: "+avg+"€";
-		}
-		textP3+="\n";
-		JTextArea text1 = new JTextArea(textP1);
-		JTextArea text2 = new JTextArea(textP2);
-		JTextArea text3 = new JTextArea(textP3);
-		text1.setSize(width, paneHeight);
-		text2.setSize(width, paneHeight);
-		text3.setSize(width, paneHeight);
+		setLayout(new GridLayout(3,1));
+	}
+	public void setTextArea(String area1, String area2, String area3) {
+		int paneHeight = HEIGHT/3;
+		JTextArea text1 = new JTextArea(area1);
+		JTextArea text2 = new JTextArea(area2);
+		JTextArea text3 = new JTextArea(area3);
+		text1.setSize(WIDTH, paneHeight);
+		text2.setSize(WIDTH, paneHeight);
+		text3.setSize(WIDTH, paneHeight);
 		text1.setEditable(false);
 		text2.setEditable(false);
 		text3.setEditable(false);
-		add(text1,BorderLayout.NORTH);
-		add(text2,BorderLayout.CENTER);
-		add(text3,BorderLayout.SOUTH);
+		add(text1);
+		add(text2);
+		add(text3);
 	}
 }
