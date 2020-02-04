345eb190cad21b1f51e9fc3c3d6bb4f8d068a6ed
selcox <sergio.munoz.lillo@gmail.com>
2020-02-04 20:16:37 +0100

    Added some shit

--- src/propra/com/sml/programacion/practicas/entregas/febrero7/ejercicio4/Ejercicio4.java
+++ src/propra/com/sml/programacion/practicas/entregas/febrero7/ejercicio4/Ejercicio4.java
@@ -1,9 +1,18 @@
 package propra.com.sml.programacion.practicas.entregas.febrero7.ejercicio4;
 
+import java.util.ArrayList;
+
 public class Ejercicio4 {
+	static ArrayList<Player> boughtPlayers = new ArrayList<Player>(); 
+	static ArrayList<Player> avaliablePlayers = new ArrayList<Player>(); 
 	public static void main(String[] args) {
 		Club myClub = new Club();
 		Ex4Graphics menu = new Ex4Graphics();
+	}
+	public static void fillAvaliable() {
+		for(int i=0;i<25;i++) {
+			avaliablePlayers.add(new Player());
+		}
 	}
 }
 
